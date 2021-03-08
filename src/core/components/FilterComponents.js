67c887f4886420a4f44f8f1246fce9bb2cfb39f3
riquemath <fredabisai@gmail.com>
2021-03-08 17:46:21 +0300

    resolve conflicts

---
+++ src/core/components/FilterComponents.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import SelectionWrapper from '../../shared/Components/SelectionWrapper/SelectionWrapper';
+import './styles/FilterComponents.css';
+import {FilterComponentTypes} from '../constants/Constants';
+    
+function FilterComponents() {
+   
+    return (
+        <Paper className="components-container" elevation={2} >
+            <SelectionWrapper type={FilterComponentTypes.ORG_UNIT} />
+            <SelectionWrapper type={FilterComponentTypes.PERIOD} />
+        </Paper>
+    )
+}
+
+export default FilterComponents
