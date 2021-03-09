170786c88b039962683095474a0e578cfd36e604
riquemath <fredabisai@gmail.com>
2021-03-09 12:49:09 +0300

    Handle on update on orgunit filter update

--- src/core/components/FilterComponents.js
+++ src/core/components/FilterComponents.js
@@ -1,17 +1,47 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import SelectionWrapper from '../../shared/Components/SelectionWrapper/SelectionWrapper';
 import './styles/FilterComponents.css';
-import {FilterComponentTypes} from '../constants/Constants';
-    
-function FilterComponents() {
-   
-    return (
-        <Paper className="components-container" elevation={2} >
-            <SelectionWrapper type={FilterComponentTypes.ORG_UNIT} />
-            <SelectionWrapper type={FilterComponentTypes.PERIOD} />
-        </Paper>
-    )
+import { FilterComponentTypes } from '../constants/Constants';
+import PeriodFilter from '../../shared/Components/PeriodFilter';
+import OrganisationUnitFilter from '../../shared/Components/OrgUnitFilter';
+
+export function FilterComponents() {
+  const [openPeriodFilter, setOpenPeriodFilter] = useState(false);
+  const [openOrgUnitFilter, setOrgUnitFilter] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+  const onUpdate = (data) => {
+    if (data) {
+      setSelectedData(data);
+    }
+    setOrgUnitFilter(false);
+  };
+
+  return (
+    <Paper className="components-container" elevation={2}>
+      <SelectionWrapper
+        onClick={(_) => setOrgUnitFilter(true)}
+        dataObj={selectedData}
+        type={FilterComponentTypes.ORG_UNIT}
+      />
+      <SelectionWrapper
+        onClick={(_) => setOpenPeriodFilter(true)}
+        type={FilterComponentTypes.PERIOD}
+      />
+      {openPeriodFilter && (
+        <PeriodFilter
+          onClose={(_) => setOpenPeriodFilter(false)}
+          onUpdate={onUpdate}
+        />
+      )}
+      {openOrgUnitFilter && (
+        <OrganisationUnitFilter
+          onClose={(_) => setOrgUnitFilter(false)}
+          onUpdate={onUpdate}
+        />
+      )}
+    </Paper>
+  );
 }
 
-export default FilterComponents
+export default FilterComponents;
