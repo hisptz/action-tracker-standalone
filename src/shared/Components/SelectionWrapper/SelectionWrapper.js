05afbc485609c3d22f33f68539f034e2a32c4b0b
riquemath <fredabisai@gmail.com>
2021-03-09 12:48:25 +0300

    Show name on updating an orgunit component

--- src/shared/Components/SelectionWrapper/SelectionWrapper.js
+++ src/shared/Components/SelectionWrapper/SelectionWrapper.js
@@ -1,24 +1,28 @@
 import Box from '@material-ui/core/Box';
 import { FilterComponentTypes } from '../../../core/constants/Constants';
+import PropTypes from 'prop-types';
 
-export function SelectionWrapper({ type, name }) {
+export function SelectionWrapper({ type, name, onClick, dataObj }) {
   const componentTypes = {
     [FilterComponentTypes.ORG_UNIT]: {
       text: 'Selected organisation unit',
+      data: dataObj ? dataObj : null,
     },
     [FilterComponentTypes.PERIOD]: {
       text: 'Selected period',
+      data: dataObj ? dataObj : null,
     },
   };
   const selectTextObj = componentTypes[type] || null;
   const selectText =
     selectTextObj && selectTextObj.text ? selectTextObj.text : '';
+    console.log({dataObj});
 
   return (
     <Box
       component="div"
       style={{
-          cursor: 'pointer'
+        cursor: 'pointer',
       }}
       borderRadius="8px"
       height="7rem"
@@ -28,6 +32,7 @@ export function SelectionWrapper({ type, name }) {
       display="flex"
       flexDirection="column"
       justifyContent="center"
+      onClick={onClick}
     >
       <strong>{selectText}</strong>
       <Box
@@ -43,7 +48,9 @@ export function SelectionWrapper({ type, name }) {
         paddingLeft="0.5em"
         textAlign="center"
       >
-        <p style={{ marginTop: 'auto', marginBottom: 'auto' }}>MOH Tanzania</p>
+        <p style={{ marginTop: 'auto', marginBottom: 'auto' }}>
+          {selectTextObj?.data?.displayName}
+        </p>
       </Box>
     </Box>
   );
@@ -51,6 +58,12 @@ export function SelectionWrapper({ type, name }) {
 SelectionWrapper.defaultProps = {
   type: '',
   name: '',
+};
+
+SelectionWrapper.poprTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default SelectionWrapper;
