d3389e46bbc504acbb7507ab187a357ecab52235
Gift Nnko <nnkogift@hotmail.com>
2021-03-09 08:10:16 +0300

    (feature/models): implemented models for the action tracker components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DataQuery } from '@dhis2/app-runtime'
+import {DataQuery} from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import classes from './App.module.css'
 
@@ -12,13 +12,13 @@ const query = {
 const MyApp = () => (
     <div className={classes.container}>
         <DataQuery query={query}>
-            {({ error, loading, data }) => {
+            {({error, loading, data}) => {
                 if (error) return <span>ERROR</span>
                 if (loading) return <span>...</span>
                 return (
                     <>
                         <h1>
-                            {i18n.t('Hello {{name}}', { name: data.me.name })}
+                            {i18n.t('Hello {{name}}', {name: data.me.name})}
                         </h1>
                         <h3>{i18n.t('Welcome to DHIS2!')}</h3>
                     </>
@@ -27,5 +27,35 @@ const MyApp = () => (
         </DataQuery>
     </div>
 )
+
+const modelQuery = {
+    indicators: {
+        resource: 'trackedEntityInstance',
+        params: {
+            program: '',
+            page: 0,
+            pageSize: 10,
+            fields: [
+                'trackedEntityInstance',
+                'attributes',
+                'enrollments[events[event, program, programStage, dataElements]]',
+            ]
+        }
+    }
+}
+
+const ModelTest = () => {
+
+    return (
+        <DataQuery query={modelQuery}>
+            {
+                ({data, loading, error}) => {
+                    console.log(data);
+                }
+            }
+        </DataQuery>
+    )
+}
+
 
 export default MyApp
