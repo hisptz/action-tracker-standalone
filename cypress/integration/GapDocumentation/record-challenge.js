203b043d1320d5fc8b51f7525f6f7dd9e62002c9
rajey <rajeyw@gmail.com>
2021-03-10 16:05:54 +0300

    add template for bbd glue code for gap management'

---
+++ cypress/integration/GapDocumentation/record-challenge.js
@@ -0,0 +1,14 @@
+/**
+ * Scenario: Record Challenges
+ */
+
+
+Given('an authorized District Planning officer', () => {}) 
+And('Form to record challenges is opened', () => {}) 
+When('selecting indicator or bottleneck with challenges', () => {}) 
+And('type in faced challenges for the indicator one after another', () => {}) 
+And('save the details', () => {}) 
+Then('district planning officer should be displayed with save confirmation message', () => {}) 
+And('saved data should be reflected in the list', () => {
+
+}) 
