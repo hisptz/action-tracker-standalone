aed91352b15ab33c42957608f9937120a99ac6a8
riquemath <fredabisai@gmail.com>
2021-03-11 14:05:04 +0300

    Add compulsory and mandatory fields

--- src/core/models/customFormField.js
+++ src/core/models/customFormField.js
@@ -1,9 +1,11 @@
 export class CustomFormField {
-  constructor({id, name, valueType, validations}) {
+  constructor({id, name, valueType, mandatory, compulsory, validations}) {
     this.id = id;
     this.name = name;
     this.valueType = valueType;
     this.validations = validations;
+    this.mandatory = mandatory;
+    this.compulsory = compulsory
   }
 }
 
