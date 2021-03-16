45cb7d5cf819d2f26e8c0230d2df9ff177914e34
riquemath <fredabisai@gmail.com>
2021-03-16 10:35:45 +0300

    Simplify function to format metadata to get fields

--- src/core/helpers/formsUtilsHelper.js
+++ src/core/helpers/formsUtilsHelper.js
@@ -1,15 +1,19 @@
-import {flattenDeep, map } from 'lodash';
-import {CustomFormField} from '../models/customFormField';
+import { flattenDeep, map } from 'lodash';
+import { CustomFormField } from '../models/customFormField';
 export const getFormattedFormMetadata = (dhisMetadataFields) => {
-    return flattenDeep(
-        map(dhisMetadataFields || [], (field) => {
-          let validations = {};
-          if ((field && field.name && field.mandatory) || field.compulsory) {
-            validations = { ...validations, required: `${field.name} is required` };
-          }
-          return field && field.id
-            ? new CustomFormField({ ...field, validations })
-            : [];
-        })
-      );
-}
+  return flattenDeep(
+    map(dhisMetadataFields || [], (field) => {
+      let validations = {};
+       validations = {...validations, ...getIsRequiredValidationFromField(field)}
+      return field && field.id
+        ? new CustomFormField({ ...field, validations })
+        : [];
+    })
+  );
+};
+
+const getIsRequiredValidationFromField = (field) => {
+  return field && field.name && (field.mandatory || field.compulsory)
+    ? { required: `${field.name} is required` }
+    : {};
+};
