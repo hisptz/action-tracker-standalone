65613a8c3bec603ab70df212f283a260bf73c9b6
riquemath <fredabisai@gmail.com>
2021-03-19 11:43:17 +0300

    Create indicators selected state

---
+++ src/core/states/indicatorsSelected.js
@@ -0,0 +1,13 @@
+import {atom} from "recoil";
+
+
+const IndicatorsSelectedState = atom({
+    key: 'indicatorsSelected',
+    default:{
+       
+    }
+})
+
+export {
+    IndicatorsSelectedState
+}
