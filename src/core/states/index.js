e50bb9a97bb365a64383fe5167addbc080f1222f
riquemath <fredabisai@gmail.com>
2021-03-19 11:38:28 +0300

    Add indicators Selected state

--- src/core/states/index.js
+++ src/core/states/index.js
@@ -1,9 +1,11 @@
 import {DimensionsState} from "./dimensions";
 import {ConfigState} from "./config";
 import {PageState} from "./page";
+import { IndicatorsSelectedState } from './indicatorsSelected';
 
 export {
     DimensionsState,
     ConfigState,
-    PageState
+    PageState,
+    IndicatorsSelectedState
 }
