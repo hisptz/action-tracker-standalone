1d21f36b51d5c61e369c8e60b770b700620947a4
riquemath <fredabisai@gmail.com>
2021-03-25 16:52:25 +0300

    Create a progress dialog component

--- src/core/states/index.js
+++ src/core/states/index.js
@@ -2,10 +2,12 @@ import {DimensionsState} from "./dimensions";
 import {ConfigState} from "./config";
 import {PageState} from "./page";
 import { IndicatorsSelectedState } from './indicatorsSelected';
+import { IndicatorProgressState } from './indicatorProgress'
 
 export {
     DimensionsState,
     ConfigState,
     PageState,
-    IndicatorsSelectedState
+    IndicatorsSelectedState,
+    IndicatorProgressState
 }
