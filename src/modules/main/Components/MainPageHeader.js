e90e57e675e372963917fb6e25f204eb7be47f12
riquemath <fredabisai@gmail.com>
2021-04-09 17:12:50 +0300

    Uncomment buttons

--- src/modules/main/Components/MainPageHeader.js
+++ src/modules/main/Components/MainPageHeader.js
@@ -1,89 +1,160 @@
-import {Button as MaterialButton, ButtonGroup, Card, Container, Grid, Typography} from "@material-ui/core";
-import React from "react";
-import {useRecoilState, useRecoilValue} from "recoil";
-import {ActionStatusState, DimensionsState, PageState, StatusFilterState} from "../../../core/states";
-import {Button, ButtonStrip, SingleSelect, SingleSelectOption} from '@dhis2/ui';
+import { useState } from 'react';
+import {
+  Button as MaterialButton,
+  ButtonGroup,
+  Card,
+  Container,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  ActionStatusState,
+  DimensionsState,
+  PageState,
+  StatusFilterState,
+} from '../../../core/states';
+import {
+  Button,
+  ButtonStrip,
+  SingleSelect,
+  SingleSelectOption,
+  FlyoutMenu,
+  MenuItem,
+} from '@dhis2/ui';
 import AddIcon from '@material-ui/icons/Add';
 import DownloadIcon from '@material-ui/icons/GetApp';
 import ColumnIcon from '@material-ui/icons/ViewColumn';
-import {useAlert} from "@dhis2/app-runtime";
-import {Period} from "@iapps/period-utilities";
-
-const PageSelector = () => {
-    const [activePage, setActivePage] = useRecoilState(PageState);
-    const {period} = useRecoilValue(DimensionsState);
-    const {show} = useAlert(({message}) => message, ({type}) => ({duration: 3000, ...type}))
-    const styles = {
-        active: {
-            background: '#2B6A5D',
-            color: '#fff',
-            textTransform: 'none'
-        },
-        inactive: {
-            textTransform: 'none'
-        }
-    }
-    const onClick = (page) => {
-        const periodInstance  = new Period().getById(_.head(period)?.id);
-       if(page === 'Tracking'){
-           if(_.has(periodInstance, 'quarterly')){
-               setActivePage(page)
-           }else{
-               show({message: 'The selected period has no quarters'})
-           }
-       }else{
-           setActivePage(page)
-       }
-    }
-
-    return (
-        <ButtonGroup>
-            <MaterialButton onClick={_ => onClick('Planning')}
-                            style={activePage === 'Planning' ? styles.active : styles.inactive}>Planning</MaterialButton>
-            <MaterialButton style={activePage === 'Tracking' ? styles.active : styles.inactive}
-                            onClick={_ => onClick('Tracking')}
-            >Tracking</MaterialButton>
-        </ButtonGroup>
-    )
+import { useAlert } from '@dhis2/app-runtime';
+import { Period } from '@iapps/period-utilities';
+import DownloadOptionsMenu from "./DownloadOptionsMenu";
+import html2canvas from 'html2canvas';
+import * as jsPDF from 'jspdf';
+import { saveAs } from 'file-saver';
+function getPagePDF() {
+  window.print();
 }
+const PageSelector = () => {
+  const [activePage, setActivePage] = useRecoilState(PageState);
+  const { period } = useRecoilValue(DimensionsState);
+  const { show } = useAlert(
+    ({ message }) => message,
+    ({ type }) => ({ duration: 3000, ...type })
+  );
+  const styles = {
+    active: {
+      background: '#2B6A5D',
+      color: '#fff',
+      textTransform: 'none',
+    },
+    inactive: {
+      textTransform: 'none',
+    },
+  };
+  const onClick = (page) => {
+    const periodInstance = new Period().getById(_.head(period)?.id);
+    if (page === 'Tracking') {
+      if (_.has(periodInstance, 'quarterly')) {
+        setActivePage(page);
+      } else {
+        show({ message: 'The selected period has no quarters' });
+      }
+    } else {
+      setActivePage(page);
+    }
+  };
 
-export default function MainPageHeader({onAddIndicatorClick}) {
-    const activePage = useRecoilValue(PageState);
-    const actionStatus = useRecoilValue(ActionStatusState);
-    const [statusFilter, setStatusFilter] = useRecoilState(StatusFilterState);
+  return (
+    <ButtonGroup>
+      <MaterialButton
+        onClick={(_) => onClick('Planning')}
+        style={activePage === 'Planning' ? styles.active : styles.inactive}
+      >
+        Planning
+      </MaterialButton>
+      <MaterialButton
+        style={activePage === 'Tracking' ? styles.active : styles.inactive}
+        onClick={(_) => onClick('Tracking')}
+      >
+        Tracking
+      </MaterialButton>
+    </ButtonGroup>
+  );
+};
 
-    return (
-        <Container maxWidth={false}>
-            <Grid container spacing={4}>
-                <Grid item container xs={6} lg={4} spacing={3}>
-                    <Grid item><Typography variant='h5'
-                                           style={{color: '#6E7A8A'}}><b>Action {activePage}</b></Typography></Grid>
-                    <Grid item> <PageSelector/></Grid>
-                </Grid>
-                <Grid container direction='row' justify='space-between' alignItems='center' item xs={12}>
-                    <Grid item container spacing={2} xs={6}>
-                        <Grid item>
-                            <Button onClick={onAddIndicatorClick} icon={<AddIcon/>}>Add Challenge</Button>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <SingleSelect clearText='Clear' clearable selected={statusFilter?.selected} placeholder='Filter by status' onChange={setStatusFilter}>
-                                {
-                                    _.map(actionStatus, status => (
-                                        <SingleSelectOption key={`${status.code}-status`} value={status.code}
-                                                            label={status.name}/>))
-                                }
-                            </SingleSelect>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={6}>
-                        {/*<ButtonStrip>*/}
-                        {/*    <Button icon={<ColumnIcon/>} >Manage Columns</Button>*/}
-                        {/*    <Button icon={<DownloadIcon/>} >Download</Button>*/}
-                        {/*</ButtonStrip>*/}
-                    </Grid>
+export default function MainPageHeader({ onAddIndicatorClick, onDownloadPDF, onDownloadExcel }) {
+  const activePage = useRecoilValue(PageState);
+  const actionStatus = useRecoilValue(ActionStatusState);
+  const [statusFilter, setStatusFilter] = useRecoilState(StatusFilterState);
+  const [reference, setReference] = useState(undefined);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  
 
-                </Grid>
+  
+
+  return (
+    <Container maxWidth={false} className="main-page-header">
+      <Grid container spacing={4}>
+        <Grid item container xs={6} lg={4} spacing={3}>
+          <Grid item>
+            <Typography variant="h5" style={{ color: '#6E7A8A' }}>
+              <b>Action {activePage}</b>
+            </Typography>
+          </Grid>
+          <Grid item>
+            {' '}
+            <PageSelector />
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+          item
+          xs={12}
+        >
+          <Grid item container spacing={2} xs={6}>
+            <Grid item>
+              <Button onClick={onAddIndicatorClick} icon={<AddIcon />}>
+                Add Challenge
+              </Button>
             </Grid>
-        </Container>
-    )
+            <Grid item xs={6}>
+              <SingleSelect
+                clearText="Clear"
+                clearable
+                selected={statusFilter?.selected}
+                placeholder="Filter by status"
+                onChange={setStatusFilter}
+              >
+                {_.map(actionStatus, (status) => (
+                  <SingleSelectOption
+                    key={`${status.code}-status`}
+                    value={status.code}
+                    label={status.name}
+                  />
+                ))}
+              </SingleSelect>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <ButtonStrip>
+                 <Button icon={<ColumnIcon/>} >Manage Columns</Button>
+              <Button onClick={(d, e) => setReference(e.currentTarget)} icon={<DownloadIcon />} >
+                Download
+              </Button>
+             
+            </ButtonStrip>
+            {
+                            reference && <DownloadOptionsMenu onDownloadExcel={onDownloadExcel} onDownloadPDF={onDownloadPDF}  reference={reference}
+                                                     onClose={_ => setReference(undefined)}/>
+                        }
+            
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
