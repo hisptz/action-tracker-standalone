db962934e948aef80706c0c7e6cb13857e21e6b1
riquemath <fredabisai@gmail.com>
2021-04-16 00:35:09 +0300

    Show status columns

--- src/shared/Components/Download/PDFTable.js
+++ src/shared/Components/Download/PDFTable.js
@@ -7,14 +7,28 @@ import {
   TableRow,
   TableCell,
 } from '@dhis2/ui';
-import FullPageLoader from '../FullPageLoader'
+import FullPageLoader from '../FullPageLoader';
+import { useRecoilValue } from 'recoil';
+import { DimensionsState, PageState } from '../../../core/states';
+import { map } from 'lodash';
+import { Period } from '@iapps/period-utilities';
+function getQuarterColumnNames(period) {
+  const periodInstance = new Period();
+  const { quarterly } = periodInstance.getById(period[0]?.id) || {};
 
+  return map(quarterly || [], (quarter) => {
+    return quarter && quarter.name ? quarter.name : '';
+  });
+}
 function PDFTable({ teiItems, isLoading }) {
   const styles = {
     listTable: {
       marginBottom: '1em',
     },
   };
+  const { orgUnit, period } = useRecoilValue(DimensionsState) || {};
+  const currentTab = useRecoilValue(PageState);
+  const quarterNames = getQuarterColumnNames(period);
   return (
     <div id="pdfTable">
       {teiItems && teiItems.length ? (
@@ -30,7 +44,15 @@ function PDFTable({ teiItems, isLoading }) {
                   <TableCellHead>Responsible Person</TableCellHead>
                   <TableCellHead>Start Date</TableCellHead>
                   <TableCellHead>End Date</TableCellHead>
-                  <TableCellHead>Status</TableCellHead>
+                  {currentTab === 'Planning' && (
+                    <TableCellHead>Status</TableCellHead>
+                  )}
+                  {currentTab === 'Tracking' &&
+                    (quarterNames || []).map((quarter, index) => {
+                      return (
+                        <TableCellHead key={index}>{quarter}</TableCellHead>
+                      );
+                    })}
                 </TableRowHead>
               </TableHead>
               <TableBody>
@@ -39,13 +61,22 @@ function PDFTable({ teiItems, isLoading }) {
                   teiItem.items.map((item, index) => {
                     return (
                       <TableRow key={item?.rowId}>
-                        <TableCell rowspan={teiItem?.items?.length}>{index === 0 && item?.indicator}</TableCell>
+                        <TableCell rowspan={teiItem?.items?.length}>
+                          {index === 0 && item?.indicator}
+                        </TableCell>
                         <TableCell>{item?.gap}</TableCell>
                         <TableCell>{item?.possibleSolution}</TableCell>
                         <TableCell>{item?.actionItem}</TableCell>
                         <TableCell>{item?.responsiblePerson}</TableCell>
                         <TableCell>{item?.startDate}</TableCell>
                         <TableCell>{item?.endDate}</TableCell>
+                        {currentTab === 'Planning' && (
+                          <TableCell>{item?.statuses[0]}</TableCell>
+                        )}
+                        {currentTab === 'Tracking' &&
+                          (item.statuses || []).map((status, index) => {
+                            return <TableCell key={index}>{status}</TableCell>;
+                          })}
                       </TableRow>
                     );
                   })}
