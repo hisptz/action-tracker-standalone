2a4675f30fefd113bf5613f3755c39ad5057a00b
riquemath <fredabisai@gmail.com>
2021-04-20 14:15:07 +0300

    Implements Tabs and their name only as per icon categories

--- src/shared/Dialogs/IconsSelectorDialog/index.js
+++ src/shared/Dialogs/IconsSelectorDialog/index.js
@@ -1,36 +1,67 @@
-import {Button, ButtonStrip, Modal, ModalActions, ModalContent, ModalTitle} from "@dhis2/ui";
+import {useState} from 'react';
+import {
+  Button,
+  ButtonStrip,
+  Modal,
+  ModalActions,
+  ModalContent,
+  ModalTitle,
+  TabBar,
+  Tab,
+} from '@dhis2/ui';
 import DHIS2Icon from '../../Components/DHIS2Icon';
 import { useDhis2Icons } from '../../../core/hooks/dhis2Icon';
 import { Dhis2IconState } from '../../../core/states';
 import { useRecoilValue } from 'recoil';
-
-function IconsSelectorDialog({onClose}) {
-    const iconsRequest = useDhis2Icons();
-    const dhis2Icons = useRecoilValue(Dhis2IconState);
-    return (
-        <Modal className="dialog-container" onClose={_ => onClose} large>
-        <ModalTitle>Select Icon</ModalTitle>
-        <ModalContent>
-          
-        </ModalContent>
-        <ModalActions>
-            <ButtonStrip>
-                <Button
-                    onClick={_ => onClose}
-                >
-                    Hide
-                </Button>
-                <Button
-                  
-                    primary
-                >
-                    Select
-                </Button>
-
-            </ButtonStrip>
-        </ModalActions>
+const iconTabs = [
+  {
+    name: 'ALL',
+  },
+  {
+    name: 'POSITIVE',
+    key: 'positive',
+  },
+  {
+    name: 'NEGATIVE',
+    key: 'negative',
+  },
+  {
+    name: 'OUTLINE',
+    key: 'outline',
+  },
+];
+function IconsSelectorDialog({ onClose }) {
+  const [selectedTab, setSelectedTab] = useState('ALL');
+  const iconsRequest = useDhis2Icons();
+  const dhis2Icons = useRecoilValue(Dhis2IconState);
+  return (
+    <Modal className="dialog-container" onClose={(_) => onClose} large>
+      <ModalTitle>Select Icon</ModalTitle>
+      <ModalContent>
+        <div
+          style={{
+            maxWidth: 700,
+          }}
+        >
+          <TabBar>
+            {(iconTabs || []).map((tab) => {
+              return (
+                <Tab key={tab.name} onClick={() => setSelectedTab(tab?.name)} selected={tab.name === selectedTab}>
+                  {tab.name} 
+                </Tab>
+              );
+            })}
+          </TabBar>
+        </div>
+      </ModalContent>
+      <ModalActions>
+        <ButtonStrip>
+          <Button onClick={(_) => onClose}>Hide</Button>
+          <Button primary>Select</Button>
+        </ButtonStrip>
+      </ModalActions>
     </Modal>
-    )
+  );
 }
 
-export default IconsSelectorDialog
+export default IconsSelectorDialog;
