7aa2727c65811f6ee90be885c6bffd50a6c8fd0f
riquemath <fredabisai@gmail.com>
2021-04-21 15:38:27 +0300

    Fetch icons according to the category

--- src/core/hooks/dhis2Icon.js
+++ src/core/hooks/dhis2Icon.js
@@ -1,33 +1,44 @@
-import { useEffect} from 'react';
-import {useDataQuery} from "@dhis2/app-runtime";
+import { useEffect } from 'react';
+import { useDataQuery } from '@dhis2/app-runtime';
 import { useSetRecoilState, useRecoilValue, useRecoilState } from 'recoil';
-import { Dhis2IconState} from '../states'
+import { Dhis2IconState } from '../states';
+import {filter} from 'lodash'
 
 const iconQuery = {
-    data: {
-        resource: 'icons',
-        params: ({}) => ({
-            totalCount: true
-        })
-    }
-}
+  data: {
+    resource: 'icons',
+    params: ({}) => ({
+      totalCount: true,
+    }),
+  },
+};
 
-export function useDhis2Icons() {
-    const {loading, data, error} = useDataQuery(iconQuery, {variables: {}});
-    const setDhis2Icons = useSetRecoilState(Dhis2IconState)
+export function useDhis2Icons({ selectedCategory = { name: 'ALL', key: '' } }) {
+  const { loading, data, error } = useDataQuery(iconQuery, { variables: {} });
+  const setDhis2Icons = useSetRecoilState(Dhis2IconState);
 
-    useEffect(() => {
-        async function setIndicatorsSelectedData() {
-          if (!loading && data && !error) {
-            const dhis2Icons = data.data ? data.data : [];
-            setDhis2Icons(dhis2Icons);
-          }
+  useEffect(() => {
+    async function setIndicatorsSelectedData() {
+      if (!loading && data && !error) {
+       
+        const dhis2Icons = data.data ? data.data : [];
+        if (selectedCategory && selectedCategory.name === 'ALL') {
+          setDhis2Icons(dhis2Icons);
+        } else if (selectedCategory && selectedCategory !== 'ALL') {
+          const filteredIcons = filter(dhis2Icons || [], (icon) => {
+            const categoryStr = /[^_]*$/.exec(icon?.key)[0] || '';
+            if (categoryStr === selectedCategory?.key) {
+              return icon;
+            }
+          });
+          setDhis2Icons(filteredIcons);
+        } else {
+          setDhis2Icons([]);
         }
-    
-        setIndicatorsSelectedData();
-      }, [loading]);
-    return {loading, error}
+      }
+    }
+
+    setIndicatorsSelectedData();
+  }, [loading, selectedCategory]);
+  return { loading, error };
 }
-
-
-
