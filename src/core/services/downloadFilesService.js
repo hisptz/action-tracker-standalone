1178ac233aa21b6c41cbdf59ba7db9eec9c21978
riquemath <fredabisai@gmail.com>
2021-04-28 18:05:47 +0300

    Change flow of data for PDF download

--- src/core/services/downloadFilesService.js
+++ src/core/services/downloadFilesService.js
@@ -1,5 +1,15 @@
 import Bottleneck from '../models/bottleneck';
-import { map, find, maxBy, filter, groupBy, mapValues, split, flattenDeep } from 'lodash';
+import {
+  map,
+  find,
+  maxBy,
+  filter,
+  groupBy,
+  mapValues,
+  split,
+  flattenDeep,
+  concat,
+} from 'lodash';
 import { GapConstants, ActionConstants } from '../constants';
 import Action from '../models/action';
 import ActionStatus from '../models/actionStatus';
@@ -63,7 +73,7 @@ export async function getPDFDownloadData({
   selectedPeriod,
   page = 1,
   pageSize = 50,
-  tableColumnsData
+  tableColumnsData,
 }) {
   const { indicators } = await engine.query(indicatorQuery, {
     variables: { ou: orgUnit?.id, page, pageSize, trackedEntityInstance: [] },
@@ -77,7 +87,7 @@ export async function getPDFDownloadData({
     engine,
     currentTab,
     selectedPeriod,
-    tableColumnsData
+    tableColumnsData,
   });
 }
 
@@ -88,7 +98,7 @@ export async function downloadExcel({
   pageSize = 50,
   currentTab,
   selectedPeriod,
-  tableColumnsData
+  tableColumnsData,
 }) {
   let teis = [];
   const { indicators } = await engine.query(indicatorQuery, {
@@ -132,11 +142,15 @@ export async function downloadExcel({
     currentTab,
     selectedPeriod,
     downloadType: 'excel',
-    tableColumnsData
+    tableColumnsData,
+    orgUnit,
   });
   const periodInstance = new Period();
-  const period= periodInstance.getById(selectedPeriod[0]?.id) || {};
-  await exportAsExcelFile(downloadData, `${orgUnit?.displayName || ''}-${period?.name || ''}  Action ${currentTab}`);
+  const period = periodInstance.getById(selectedPeriod[0]?.id) || {};
+  await exportAsExcelFile(
+    downloadData,
+    `${orgUnit?.displayName || ''}-${period?.name || ''}  Action ${currentTab}`
+  );
 }
 
 async function getDownloadData({
@@ -146,8 +160,16 @@ async function getDownloadData({
   selectedPeriod,
   pageSize = 50,
   currentTab,
-  tableColumnsData
+  tableColumnsData,
 }) {
+  const { gapsTable, actionsTable, solutionsTable } = tableColumnsData;
+
+  const columns = filter(concat(
+    [],
+    gapsTable?.columns || [],
+    solutionsTable?.columns || [],
+    actionsTable?.columns || []
+  ) || [], columnItem => columnItem.visible) ;
   const pager = indicators && indicators.pager ? indicators.pager : {};
   const { pageCount } = pager;
   let teis = [];
@@ -179,7 +201,9 @@ async function getDownloadData({
     downloadType: 'pdf',
     selectedPeriod,
     currentTab,
-    tableColumnsData
+    tableColumnsData,
+    orgUnit,
+    columns
   });
   const groupedFormattedDataObj = mapValues(
     groupBy(downloadFormattedData || [], 'id'),
@@ -194,6 +218,7 @@ async function getDownloadData({
         ? {
             id: formattedDataGroupKey,
             items: groupedFormattedDataObj[formattedDataGroupKey],
+            headers:  columns
           }
         : [];
     }
@@ -345,7 +370,9 @@ async function formatDataForDownload({
   downloadType = 'excel',
   currentTab = 'Planning',
   selectedPeriod,
-  tableColumnsData
+  tableColumnsData,
+  orgUnit,
+  columns
 }) {
   let formatted = [];
   if (formattedTeis && formattedTeis.length) {
@@ -363,9 +390,9 @@ async function formatDataForDownload({
                     currentTab,
                     downloadType,
                     selectedPeriod,
-                    tableColumnsData
+                    tableColumnsData,
                   });
-                  
+
                   const formattedObj = getRowObjectByDownloadType({
                     downloadType,
                     tei,
@@ -374,9 +401,11 @@ async function formatDataForDownload({
                     possibleSolution,
                     actionStatusesObj,
                     indicatorName,
+                    tableColumnsData,
                     selectedPeriod,
+                    orgUnit,
+                    columns
                   });
-
                   formatted = [...formatted, formattedObj];
                 }
               }
@@ -386,53 +415,57 @@ async function formatDataForDownload({
       }
     }
   }
+
   return formatted;
 }
 
-
 function getRowObjectByDownloadType({
   downloadType,
-  tei,
   gap,
   action,
   possibleSolution,
   actionStatusesObj,
   indicatorName,
-  selectedPeriod,
+  tableColumnsData,
+  orgUnit,
+  tei,
+  columns
 }) {
+  // const { gapsTable, actionsTable, solutionsTable } = tableColumnsData;
 
-  let statuses =  map(Object.keys(actionStatusesObj) || [], statusKey => {
-    return statusKey ? actionStatusesObj[statusKey] : '';
-  })
- 
-  
-  
-  return downloadType === 'excel'
-    ? {
-        Indicator: indicatorName,
-        Gap: gap?.description || '',
-        'Possible Solutions': possibleSolution?.solution || '',
-        'Action Items': action?.description || '',
-        'Responsible Person': action?.responsiblePerson || '',
-        'Start Date': action?.startDate || '',
-        'End Date': action?.endDate || '',
-        ...actionStatusesObj,
-      }
-    : downloadType === 'pdf'
-    ? {
-        id: tei?.id || '',
-        indicator: indicatorName,
-        gapId: gap?.id || '',
-        gap: gap?.description || '',
-        possibleSolution: possibleSolution?.solution || '',
-        actionItem: action?.description || '',
-        responsiblePerson: action?.responsiblePerson || '',
-        startDate: action?.startDate || '',
-        endDate: action?.endDate || '',
-        rowId: uuidv4(),
-        statuses
-      }
-    : {};
+  // const columns = concat(
+  //   [],
+  //   gapsTable?.columns || [],
+  //   solutionsTable?.columns || [],
+  //   actionsTable?.columns || []
+  // );
+
+  if (downloadType === 'pdf') {
+    const statuses = map(Object.keys(actionStatusesObj) || [], (statusKey) => {
+      return statusKey ? actionStatusesObj[statusKey] : '';
+    });
+    return getPdfRowData({
+      columns,
+      gap,
+      action,
+      possibleSolution,
+      orgUnit,
+      indicatorName,
+      statuses,
+      tei,
+    });
+  } else if (downloadType === 'excel') {
+    return getExcelRowData({
+      columns,
+      gap,
+      action,
+      possibleSolution,
+      orgUnit,
+      indicatorName,
+    });
+  } else {
+    return {};
+  }
 }
 
 function getActionStatusObject({
@@ -440,54 +473,53 @@ function getActionStatusObject({
   currentTab,
   downloadType,
   selectedPeriod,
-  tableColumnsData
+  tableColumnsData,
 }) {
   let actionStatusesObj = {};
   const { actionStatus } = action || {};
- 
+
   const statusObj = maxBy(
     actionStatus || [],
     (actionStatusItem) => new Date(actionStatusItem?.reviewDate)
   );
   if (currentTab === 'Planning') {
-  actionStatusesObj =
-    statusObj && statusObj.status && downloadType === 'excel'
-      ? { ...actionStatusesObj, Status: statusObj.status }
-      : statusObj && statusObj.status && downloadType === 'pdf'
-      ? { ...actionStatusesObj, status: statusObj.status }
-      : {};
+    actionStatusesObj =
+      statusObj && statusObj.status && downloadType === 'excel'
+        ? { ...actionStatusesObj, Status: statusObj.status }
+        : statusObj && statusObj.status && downloadType === 'pdf'
+        ? { ...actionStatusesObj, status: statusObj.status }
+        : {};
   }
- if (currentTab === 'Tracking') {
-  
-    
+  if (currentTab === 'Tracking') {
     const periodInstance = new Period();
     const { quarterly } = periodInstance.getById(selectedPeriod[0]?.id) || {};
 
-     
-   const {columns} = tableColumnsData?.actionStatusTable || {columns: []};
+    const { columns } = tableColumnsData?.actionStatusTable || { columns: [] };
 
-   if(columns?.length) {
-    
-    for (const column of columns) {
+    if (columns?.length) {
+      for (const column of columns) {
+        const periodValues = periodInstance.getById(column?.id);
+        const formattedStartDate = new Date(validDate(periodValues.startDate));
+        const formattedEndDate = new Date(validDate(periodValues.endDate));
+        const dateInGivenColumn = find(actionStatus || [], (status) => {
+          const formattedReviewDate = new Date(status?.reviewDate);
 
-      const periodValues = periodInstance.getById(column?.id)
-      const formattedStartDate = new Date(validDate(periodValues.startDate));
-      const formattedEndDate = new Date(validDate(periodValues.endDate));
-      const dateInGivenColumn = find(actionStatus || [], status => {
-        const formattedReviewDate = new Date(status?.reviewDate);
-       
-         if(formattedReviewDate >= formattedStartDate &&
-          formattedReviewDate <=formattedEndDate) {
+          if (
+            formattedReviewDate >= formattedStartDate &&
+            formattedReviewDate <= formattedEndDate
+          ) {
             return status;
           }
-      });
+        });
 
-      actionStatusesObj = { ...actionStatusesObj, [column?.name]: dateInGivenColumn?.status || '' };
-     }
-   }
-
+        actionStatusesObj = {
+          ...actionStatusesObj,
+          [column?.name]: dateInGivenColumn?.status || '',
+        };
+      }
+    }
   }
- 
+
   return actionStatusesObj;
 }
 function validDate(date) {
@@ -500,10 +532,157 @@ function validDate(date) {
       dateItemIndex++
     ) {
       dateStr =
-        dateItemIndex === (dateArr.length - 1) 
+        dateItemIndex === dateArr.length - 1
           ? `${dateStr}${dateArr[dateItemIndex]}`
           : `${dateStr}${dateArr[dateItemIndex]}-`;
     }
   }
   return dateStr;
+}
+
+function getPdfRowData({
+  columns,
+  gap,
+  orgUnit,
+  possibleSolution,
+  action,
+  indicatorName,
+  statuses,
+  tei,
+}) {
+  let pdfObj = {
+    id: tei?.id || '',
+    indicator: { value: indicatorName, displayName: 'Indicator' },
+  };
+  for (const column of columns) {
+    if (column.visible) {
+      switch (column?.name) {
+        case 'gap':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({ value: gap?.description, column }),
+          };
+          break;
+        case 'orgUnit':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({ value: orgUnit?.displayName, column }),
+          };
+          break;
+        case 'possibleSolution':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({
+              value: possibleSolution?.solution,
+              column,
+            }),
+          };
+          break;
+        case 'action':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({ value: action?.description, column }),
+          };
+          break;
+        case 'responsiblePerson':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({
+              value: action?.responsiblePerson,
+              column,
+            }),
+          };
+          break;
+        case 'startDate':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({ value: action?.startDate, column }),
+          };
+          break;
+        case 'endDate':
+          pdfObj = {
+            ...pdfObj,
+            ...getPdfColumnKeyData({ value: action?.endDate, column }),
+          };
+          break;
+        default:
+          pdfObj = { ...pdfObj };
+          break;
+      }
+
+      pdfObj = { ...pdfObj, statuses, rowId: uuidv4() };
+    }
+  }
+  return pdfObj;
+}
+function getPdfColumnKeyData({ value, column }) {
+  return column?.name
+    ? {
+        [column?.name]: { value, displayName: column?.displayName || '' },
+      }
+    : {};
+}
+function getExcelRowData({
+  columns,
+  gap,
+  orgUnit,
+  possibleSolution,
+  action,
+  indicatorName,
+}) {
+  let excelObj = { Indicator: indicatorName };
+  for (const column of columns) {
+    if (column.visible) {
+      switch (column?.name) {
+        case 'gap':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: gap?.description || '',
+          };
+          break;
+        case 'orgUnit':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: orgUnit?.displayName || '',
+          };
+          break;
+        case 'possibleSolution':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: possibleSolution?.solution || '',
+          };
+          break;
+        case 'action':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: action?.description || '',
+          };
+          break;
+        case 'responsiblePerson':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: action?.responsiblePerson || '',
+          };
+          break;
+        case 'startDate':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: action?.startDate || '',
+          };
+          break;
+        case 'endDate':
+          excelObj = {
+            ...excelObj,
+            [column?.displayName]: action?.endDate || '',
+          };
+          break;
+        default:
+          excelObj = { ...excelObj };
+          break;
+      }
+
+      excelObj = Object.assign(excelObj, actionStatusesObj);
+    }
+  }
+  return excelObj;
 }
