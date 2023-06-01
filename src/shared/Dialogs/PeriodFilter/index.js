86f835077203d3ecafafbaf1d2a18bc8111e5f15
gian <nnkogift@hotmail.com>
2023-06-01 11:10:17 +0300

    fix(period selector): Fixes
    - Fixed issues with period selector failing to include current year

--- src/shared/Dialogs/PeriodFilter/index.js
+++ src/shared/Dialogs/PeriodFilter/index.js
@@ -1,27 +1,25 @@
 import {AlertBar, Button, ButtonStrip, Modal, ModalActions, ModalContent, ModalTitle,} from '@dhis2/ui';
 import React, {useMemo, useState} from 'react';
-import {Period, PeriodType} from "@iapps/period-utilities";
 import {useSetting} from "@dhis2/app-service-datastore";
 import * as _ from "lodash";
 import {useAlert} from "@dhis2/app-runtime";
 import DataStoreConstants from "../../../core/constants/datastore";
 import i18n from '@dhis2/d2-i18n'
-import CalendarSpecificPeriodDimension from "./Components/CalendarSpecificPeriodDimension";
-import {CalendarTypes} from "./Components/CalendarSpecificPeriodDimension/constants/calendar";
-
-import {PeriodDimension} from '@dhis2/analytics'
+import {PeriodSelector} from "@hisptz/dhis2-ui";
+import {PeriodTypeCategory, PeriodUtility} from "@hisptz/dhis2-utils";
 
 export default function PeriodFilter({onClose, onUpdate, initialPeriods}) {
-    const [selectedPeriods, setSelectedPeriods] = useState(initialPeriods && [initialPeriods]);
+    const [selectedPeriods, setSelectedPeriods] = useState(initialPeriods ? [initialPeriods.id] : []);
     const [error, setError] = useState();
-    const periodInstance = new Period();
-    periodInstance.setPreferences({allowFuturePeriods: true});
     const [planningPeriod] = useSetting(DataStoreConstants.PLANNING_PERIOD_KEY, {global: true})
     const {show} = useAlert(({message}) => message, ({type}) => ({duration: 3000, ...type}))
     // useEffect(() => generateErrorAlert(show, error), [error])
     const excludePeriodTypes = useMemo(() => {
-        const periodsTypes = new PeriodType().get()
-        return _.filter(periodsTypes, ({id}) => (id !== planningPeriod))?.map(({id}) => id)
+        const periodUtility = new PeriodUtility();
+        periodUtility.setYear(new Date().getFullYear());
+        periodUtility.setCategory(PeriodTypeCategory.FIXED);
+        const periodsTypes = periodUtility.periodTypes;
+        return _.filter(periodsTypes, ({id}) => (id.toLowerCase() !== planningPeriod.toLowerCase()))?.map(({id}) => id)
     }, [planningPeriod]);
     const styles = {
         errorText: {
@@ -31,11 +29,14 @@ export default function PeriodFilter({onClose, onUpdate, initialPeriods}) {
         }
     }
 
+    console.log(excludePeriodTypes)
+
     const onUpdateClick = () => {
         if (!_.isEmpty(selectedPeriods)) {
-            const periodObject = periodInstance.getById(_.head(selectedPeriods)?.id);
-            if (periodObject?.type === planningPeriod) {
+            const periodObject = PeriodUtility.getPeriodById(_.head(selectedPeriods));
+            if (periodObject?.type.id.toLowerCase() === planningPeriod.toLowerCase()) {
                 if (onUpdate) {
+                    console.log("Updating")
                     onUpdate(periodObject);
                 } else {
                     onClose();
@@ -48,8 +49,9 @@ export default function PeriodFilter({onClose, onUpdate, initialPeriods}) {
             show({message: i18n.t('Please select a period.')})
         }
     }
+
     return (
-        <Modal position={'middle'} open={open} onClose={onClose} dataTest='period-filter'>
+        <Modal position={'middle'} hide={!open} onClose={onClose} dataTest='period-filter'>
             <ModalTitle>
                 {
                     i18n.t('Period')
@@ -58,22 +60,18 @@ export default function PeriodFilter({onClose, onUpdate, initialPeriods}) {
             <ModalContent>
                 <p><b>{i18n.t('Planning Period')}: </b> {i18n.t('{{ planningPeriod }} ', {planningPeriod})}</p>
                 {error &&
-                <AlertBar backGroundColor={'red'} color={'blue'} duration={2000}>
-                    {i18n.t('{{ error }}', {error})}
-                </AlertBar>
+                    <AlertBar backGroundColor={'red'} color={'blue'} duration={2000}>
+                        {i18n.t('{{ error }}', {error})}
+                    </AlertBar>
 
                 }
-                <CalendarSpecificPeriodDimension
-                    calendar={CalendarTypes.GREGORIAN}
-                    excludedPeriodTypes={excludePeriodTypes}
-                    onSelect={(period) => {
-                        const items = period?.items || [];
-                        if (items?.length && items.length > 1) {
-                            items.shift();
-                        }
-                        setSelectedPeriods(items)
-                    }}
-                    selectedPeriods={selectedPeriods ?? []}
+                <PeriodSelector
+                    excludedPeriodTypes={excludePeriodTypes.filter((id) => id !== 'DAILY')}
+                    enablePeriodSelector
+                    excludeRelativePeriods
+                    singleSelection
+                    selectedPeriods={[...selectedPeriods]}
+                    onSelect={({items}) => setSelectedPeriods(items)}
                 />
             </ModalContent>
             <ModalActions>
