71871e4c39d5315fc06cd2dac732a59219b8f55d
Gift Nnko <nnkogift@hotmail.com>
2023-08-05 22:09:20 +0300

    feat(tests): Setup
    - Setup cypress testing

--- cypress/e2e/common/users-login.js
+++ cypress/e2e/common/users-login.js
@@ -1,22 +1,9 @@
 /// <reference types=”cypress” />
-import {Given} from "@badeball/cypress-cucumber-preprocessor"
+import { Given } from '@badeball/cypress-cucumber-preprocessor'
 
-const baseUrl = Cypress.env('dhis2BaseUrl')
-const username = Cypress.env('dhis2Username')
-const password = Cypress.env('dhis2Password')
-
-
-export function login(){
-    cy.get('#server').clear().type(baseUrl)
-    cy.get('#j_username').type(username)
-    cy.get('#j_password').type(password)
-    cy.get('[data-test="dhis2-adapter-loginsubmit"]').click()
-}
 Given(/^an authorized District Planning officer$/, function () {
     cy.visit('/');
-   login();
 });
 Given('an authorized CHMT member', () => {
     cy.visit("/");
-    login()
 })
--- cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add("login", (username, password) => {
-    cy.get('#j_username').type(username);
-    cy.get('#j_password').type(password);
-    cy.get("[data-test=dhis2-adapter-loginsubmit]").should("be.visible").click();
-
-});
