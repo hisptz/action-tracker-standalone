268e87cd8485582e288e151c790e18877bdac436
Gift Nnko <nnkogift@hotmail.com>
2023-08-09 11:21:32 +0300

    fix(config): Improvements
    - Fixed issues with redirection after save completed

--- src/modules/Config/components/SaveArea.tsx
+++ src/modules/Config/components/SaveArea.tsx
@@ -7,46 +7,64 @@ import { DATASTORE_NAMESPACE } from '../../../shared/constants/meta'
 import { useAlert, useDataMutation } from '@dhis2/app-runtime'
 import { useUpdateMetadata } from '../../../shared/hooks/metadata'
 import { useConfiguration } from '../../../shared/hooks/config'
+import { useNavigate } from 'react-router-dom'
 
 const mutation = {
     resource: `dataStore/${DATASTORE_NAMESPACE}`,
-    type: "update",
-    id: ({data}: { data: Config }) => data?.id,
-    data: ({data}: { data: Config }) => data
+    type: 'update',
+    id: ({ data }: { data: Config }) => data?.id,
+    data: ({ data }: { data: Config }) => data
 }
 
-export function SaveArea() {
+export function SaveArea () {
     const { reset } = useConfiguration()
-    const {updateMetadataFromConfig, uploadingMetadata} = useUpdateMetadata();
-    const {show, hide} = useAlert(({message}) => message, ({type}) => ({...type, duration: 3000}))
-    const form = useFormContext<Config>();
-    const [update, {loading}] = useDataMutation(mutation as any, {
+    const navigate = useNavigate()
+    const {
+        updateMetadataFromConfig,
+        uploadingMetadata
+    } = useUpdateMetadata()
+    const {
+        show,
+        hide
+    } = useAlert(({ message }) => message, ({ type }) => ({
+        ...type,
+        duration: 3000
+    }))
+    const form = useFormContext<Config>()
+    const [update, { loading }] = useDataMutation(mutation as any, {
         onError: (error) => {
-            show({message: `${i18n.t("Error saving changes") as string}: ${error.message}`, type: {critical: true}});
-             new Promise((resolve) => setTimeout(resolve, 5000)).then(hide).catch(console.error);
+            show({
+                message: `${i18n.t('Error saving changes') as string}: ${error.message}`,
+                type: { critical: true }
+            })
+            new Promise((resolve) => setTimeout(resolve, 5000)).then(hide).catch(console.error)
         }
     })
     const onSave = async (data: Config) => {
-        const response = await updateMetadataFromConfig(data);
+        const response = await updateMetadataFromConfig(data)
         if (response === undefined) {
-            return;
+            return
         }
-        await update({data});
+        await update({ data })
         show({
             message: i18n.t('Changes saved successfully'),
             type: { success: true }
         })
         reset()
+        navigate('/', { replace: true })
     }
     return (
-        <div style={{display: "flex", justifyContent: "flex-end"}} className="row gap-16">
+        <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end'
+        }} className="row gap-16">
             <ButtonStrip>
                 <Button disabled={!form.formState.isDirty}>
-                    {i18n.t("Reset")}
+                    {i18n.t('Reset')}
                 </Button>
                 <Button loading={loading || uploadingMetadata} onClick={form.handleSubmit(onSave)} primary
                         disabled={!form.formState.isDirty || loading || uploadingMetadata}>
-                    {loading ? i18n.t("Saving") : i18n.t("Save changes")}
+                    {uploadingMetadata ? i18n.t('Updating metadata...') : loading ? i18n.t('Saving...') : i18n.t('Save changes')}
                 </Button>
             </ButtonStrip>
         </div>
