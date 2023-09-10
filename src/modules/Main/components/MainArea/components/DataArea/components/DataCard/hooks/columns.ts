340cb9923cc629a5f83afb8fce52f7583f963ce3
Gift Nnko <nnkogift@hotmail.com>
2023-09-10 18:49:44 +0300

    fix(tracking table): Fixes
    - Fixed issues with current periods not appearing in tracking table

--- src/modules/Main/components/MainArea/components/DataArea/components/DataCard/hooks/columns.ts
+++ src/modules/Main/components/MainArea/components/DataArea/components/DataCard/hooks/columns.ts
@@ -24,7 +24,7 @@ export function useTrackingColumns () {
         if (!periodTypeId) return []
 
         const periodType = new PeriodUtility().setCategory(PeriodTypeCategory.FIXED).setYear(period?.get()?.endDate.getFullYear() ?? new Date().getFullYear()).getPeriodType(periodTypeId)
-        return periodType?.periods.filter((pe) => period?.interval.engulfs(pe.interval)) ?? []
+        return periodType?.periods.filter((pe) => period?.interval.engulfs(pe.interval) || pe.start.diffNow('days').days <= 0) ?? []
     }, [])
 
     return useMemo(() => {
