cd4360a98e80d024ebef5810448fbfc7a98e71c0
Gift Nnko <nnkogift@hotmail.com>
2023-12-05 15:30:44 +0300

    fix(tracking table): Fixes
    - Fixed issues with date formatting for fields of type date in tracking table

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
 
 export function formatDate (dateString: string): string {
-    return DateTime.fromJSDate(new Date(dateString)).toLocaleString()
+    return DateTime.fromJSDate(new Date(dateString)).toFormat('dd MMMM yyyy')
 }
