46dfa76bb3d0c8861a97bb525c605f1b666b5d39
Gift Nnko <nnkogift@hotmail.com>
2024-06-24 14:14:07 +0300

    fix: tracker apis
    Fixed issues caused with tracker api changes for v41

--- src/modules/Main/components/MainArea/components/DataArea/components/DataCard/components/TrackingTable/hooks/data.ts
+++ src/modules/Main/components/MainArea/components/DataArea/components/DataCard/components/TrackingTable/hooks/data.ts
@@ -1,68 +1,65 @@
-import { useDataQuery } from '@dhis2/app-runtime'
-import { useConfiguration } from '../../../../../../../../../../../shared/hooks/config'
-import { useDimensions } from '../../../../../../../../../../../shared/hooks'
-import { useMemo } from 'react'
-import { WebapiControllerTrackerView_Event } from '../../../../../../../../../../../shared/types/dhis2'
+import { useDataQuery } from "@dhis2/app-runtime";
+import { useConfiguration } from "../../../../../../../../../../../shared/hooks/config";
+import { useDimensions } from "../../../../../../../../../../../shared/hooks";
+import { useMemo } from "react";
+import { WebapiControllerTrackerView_Event } from "../../../../../../../../../../../shared/types/dhis2";
 
 const trackingQuery: any = {
-    events: {
-        resource: 'tracker/events',
-        params: ({
-                     trackedEntity,
-                     program,
-                     programStage,
-                     start,
-                     end
-                 }: {
-            trackedEntity: string;
-            program: string;
-            programStage: string;
-            start: string;
-            end: string;
-        }) => {
-            return {
-                trackedEntity,
-                program,
-                programStage,
-                occurredAfter: start,
-                occurredBefore: end,
-                paging: false
-            }
-        }
-    }
-}
+	events: {
+		resource: "tracker/events",
+		params: ({
+			trackedEntity,
+			program,
+			programStage,
+			start,
+			end,
+		}: {
+			trackedEntity: string;
+			program: string;
+			programStage: string;
+			start: string;
+			end: string;
+		}) => {
+			return {
+				trackedEntity,
+				program,
+				programStage,
+				occurredAfter: start,
+				occurredBefore: end,
+				paging: false,
+			};
+		},
+	},
+};
 
 type Response = {
-    events: {
-        instances: WebapiControllerTrackerView_Event[];
-    }
-}
+	events: {
+		instances: WebapiControllerTrackerView_Event[];
+		events?: WebapiControllerTrackerView_Event[];
+	};
+};
 
-export function useTrackingTableData ({ instance }: {
-    instance: any;
-}) {
-    const { config } = useConfiguration()
-    const { period } = useDimensions()
-    const {
-        loading,
-        data,
-        refetch
-    } = useDataQuery<Response>(trackingQuery, {
-        variables: {
-            program: instance.program,
-            programStage: config?.action.statusConfig?.id,
-            trackedEntity: instance.trackedEntity,
-            start: period?.start.toFormat('yyyy-MM-dd'),
-            end: period?.end.toFormat('yyyy-MM-dd')
-        }
-    })
+export function useTrackingTableData({ instance }: { instance: any }) {
+	const { config } = useConfiguration();
+	const { period } = useDimensions();
+	const { loading, data, refetch } = useDataQuery<Response>(trackingQuery, {
+		variables: {
+			program: instance.program,
+			programStage: config?.action.statusConfig?.id,
+			trackedEntity: instance.trackedEntity,
+			start: period?.start.toFormat("yyyy-MM-dd"),
+			end: period?.end.toFormat("yyyy-MM-dd"),
+		},
+	});
 
-    const events = useMemo(() => data?.events?.instances ?? [], [data?.events])
+	const events = useMemo(
+		() => data?.events?.instances ?? data?.events?.events ?? [],
+		[data?.events],
+	);
 
-    return {
-        loading,
-        events,
-        refetch
-    }
-
+	return {
+		loading,
+		events,
+		refetch,
+	};
 }
