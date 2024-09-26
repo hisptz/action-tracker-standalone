e9565f547b97ec095725f00b678863434f16d4cc
Gift Nnko <nnkogift@gmail.com>
2024-09-26 08:17:26 +0300

    fix: tracking
    Fixed issue with missing start and end date on an activity causes app to crash

--- src/modules/Main/components/MainArea/components/DataArea/components/DataCard/components/TrackingTable/components/ActionStatus/hooks/date.ts
+++ src/modules/Main/components/MainArea/components/DataArea/components/DataCard/components/TrackingTable/components/ActionStatus/hooks/date.ts
@@ -1,6 +1,6 @@
 import {
-	TrackedEntity,
-	WebapiControllerTrackerView_Event,
+    TrackedEntity,
+    WebapiControllerTrackerView_Event,
 } from "../../../../../../../../../../../../../shared/types/dhis2";
 import { useConfiguration } from "../../../../../../../../../../../../../shared/hooks/config";
 import { DateTime, Interval } from "luxon";
@@ -8,39 +8,39 @@ import { Config } from "../../../../../../../../../../../../../shared/schemas/co
 import { FixedPeriod } from "@dhis2/multi-calendar-dates/build/types/period-calculation/types";
 
 function getCompleteOrCancelledStatus({
-	config,
-	events,
-	period,
+    config,
+    events,
+    period,
 }: {
 	events: WebapiControllerTrackerView_Event[];
 	config: Config;
 	period: FixedPeriod & { start: DateTime; end: DateTime };
 }) {
-	const statusDataElement =
+    const statusDataElement =
 		config.action.statusConfig.stateConfig.dataElement;
-	const completeStatusCode =
+    const completeStatusCode =
 		config.action.statusConfig.stateConfig.completeOptionCode;
-	const cancelledStatusCode =
+    const cancelledStatusCode =
 		config.action.statusConfig.stateConfig.cancelOptionCode;
 
-	return events.find((event) => {
-		const status = event.dataValues.find((dataValue) => {
-			return dataValue.dataElement === statusDataElement;
-		});
-		return (
-			(status?.value === completeStatusCode ||
+    return events.find((event) => {
+        const status = event.dataValues.find((dataValue) => {
+            return dataValue.dataElement === statusDataElement;
+        });
+        return (
+            (status?.value === completeStatusCode ||
 				status?.value === cancelledStatusCode) &&
 			period.start
-				.diff(DateTime.fromJSDate(new Date(event.occurredAt)))
-				.shiftTo("days").days > 0
-		);
-	});
+			    .diff(DateTime.fromJSDate(new Date(event.occurredAt)))
+			    .shiftTo("days").days > 0
+        );
+    });
 }
 
 export function useShowActionTracking({
-	action,
-	period,
-	events,
+    action,
+    period,
+    events,
 }: {
 	action: TrackedEntity;
 	period: FixedPeriod & {
@@ -50,45 +50,53 @@ export function useShowActionTracking({
 	};
 	events: WebapiControllerTrackerView_Event[];
 }) {
-	const { config } = useConfiguration();
+    const { config } = useConfiguration();
 
-	if (!config) {
-		return;
-	}
+    if (!config) {
+        return;
+    }
 
-	const completeOrCancelledStatus = getCompleteOrCancelledStatus({
-		config,
-		period,
-		events,
-	});
+    const completeOrCancelledStatus = getCompleteOrCancelledStatus({
+        config,
+        period,
+        events,
+    });
 
-	if (completeOrCancelledStatus) {
-		return false;
-	}
+    if (completeOrCancelledStatus) {
+        return false;
+    }
 
-	const actionConfigFields = config?.action.fields;
-	const startDateField = actionConfigFields?.find(
-		(field) => field.isStartDate,
-	);
-	const endDateField = actionConfigFields?.find((field) => field.isEndDate);
-	const actionStartDate = DateTime.fromJSDate(
-		new Date(
-			action.attributes.find(
-				({ attribute }) => startDateField?.id === attribute,
-			)?.value as string,
-		),
-	);
-	const actionEndDate = DateTime.fromJSDate(
-		new Date(
-			action.attributes.find(
-				({ attribute }) => endDateField?.id === attribute,
-			)?.value as string,
-		),
-	);
+    const actionConfigFields = config?.action.fields;
+    const startDateField = actionConfigFields?.find(
+        (field) => field.isStartDate,
+    );
 
-	const periodsIntersect = period.interval.intersection(
-		Interval.fromDateTimes(actionStartDate, actionEndDate),
-	);
+    const actionStartJSDate = new Date(
+		action.attributes.find(
+		    ({ attribute }) => startDateField?.id === attribute,
+		)?.value as string,
+    );
 
-	return !!periodsIntersect;
+    const actionEndJSDate = new Date(
+		action.attributes.find(
+		    ({ attribute }) => endDateField?.id === attribute,
+		)?.value as string,
+    );
+
+    if (
+        isNaN(actionEndJSDate.valueOf()) ||
+		isNaN(actionStartJSDate.valueOf())
+    ) {
+        return false;
+    }
+
+    const endDateField = actionConfigFields?.find((field) => field.isEndDate);
+    const actionStartDate = DateTime.fromJSDate(actionStartJSDate);
+    const actionEndDate = DateTime.fromJSDate(actionEndJSDate);
+
+    const periodsIntersect = period.interval.intersection(
+        Interval.fromDateTimes(actionStartDate, actionEndDate),
+    );
+
+    return !!periodsIntersect;
 }
--- src/modules/Main/components/MainArea/components/DataArea/components/DataCard/hooks/columns.ts
+++ src/modules/Main/components/MainArea/components/DataArea/components/DataCard/hooks/columns.ts
@@ -1,23 +1,23 @@
 import { useConfiguration } from "../../../../../../../../../shared/hooks/config";
 import { useCallback, useEffect, useMemo } from "react";
 import {
-	type ActionTrackingColumnStateConfig,
-	ColumnState,
-	type ColumnStateConfig,
-	VisibleColumnState,
+    type ActionTrackingColumnStateConfig,
+    ColumnState,
+    type ColumnStateConfig,
+    VisibleColumnState,
 } from "../state/columns";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { useWindowSize } from "usehooks-ts";
 import {
-	useDimensions,
-	usePageType,
+    useDimensions,
+    usePageType,
 } from "../../../../../../../../../shared/hooks";
 import i18n from "@dhis2/d2-i18n";
 import { clamp, compact, flatten, range, uniqBy } from "lodash";
 import { generateFixedPeriods } from "@dhis2/multi-calendar-dates";
 import {
-	FixedPeriod,
-	PeriodType,
+    FixedPeriod,
+    PeriodType,
 } from "@dhis2/multi-calendar-dates/build/types/period-calculation/types";
 import { DateTime, Interval } from "luxon";
 import { useCalendar } from "../../../../../../../../../shared/hooks/calendar";
@@ -37,204 +37,204 @@ import { useCalendar } from "../../../../../../../../../shared/hooks/calendar";
  *  */
 
 export function useTrackingPeriods() {
-	const { config } = useConfiguration();
-	const { period } = useDimensions();
-	const calendar = useCalendar();
+    const { config } = useConfiguration();
+    const { period } = useDimensions();
+    const calendar = useCalendar();
 
-	return useMemo(() => {
-		const { general } = config ?? {};
-		const periodTypeId = general?.period.tracking;
-		const planningPeriodTypeId = general?.period.planning;
+    return useMemo(() => {
+        const { general } = config ?? {};
+        const periodTypeId = general?.period.tracking;
+        const planningPeriodTypeId = general?.period.planning;
 
-		if (periodTypeId === planningPeriodTypeId) {
-			return compact([period]);
-		}
-		if (!periodTypeId || !period) return [];
+        if (periodTypeId === planningPeriodTypeId) {
+            return compact([period]);
+        }
+        if (!periodTypeId || !period) return [];
 
-		let periods: FixedPeriod[] = [];
+        let periods: FixedPeriod[] = [];
 
-		if (period.start.year === period.end.year) {
-			periods =
+        if (period.start.year === period.end.year) {
+            periods =
 				generateFixedPeriods({
-					year: period.start.year,
-					calendar,
-					periodType: periodTypeId as PeriodType,
+				    year: period.start.year,
+				    calendar,
+				    periodType: periodTypeId as PeriodType,
 				}) ?? [];
-		} else {
-			periods =
+        } else {
+            periods =
 				compact(
-					flatten(
-						range(period.start.year, period.end.year + 1).map(
-							(year) => {
-								return generateFixedPeriods({
-									year: year,
-									calendar,
-									periodType: periodTypeId as PeriodType,
-								});
-							},
-						) ?? [],
-					),
+				    flatten(
+				        range(period.start.year, period.end.year + 1).map(
+				            (year) => {
+				                return generateFixedPeriods({
+				                    year: year,
+				                    calendar,
+				                    periodType: periodTypeId as PeriodType,
+				                });
+				            },
+				        ) ?? [],
+				    ),
 				) ?? [];
-		}
+        }
 
-		const filteredPeriods =
+        const filteredPeriods =
 			compact(
-				uniqBy(periods, "id").filter((pe) =>
-					period.interval.engulfs(
-						Interval.fromDateTimes(
-							DateTime.fromFormat(
-								pe.startDate,
-								"yyyy-MM-dd",
-							).startOf("day"),
-							DateTime.fromFormat(pe.endDate, "yyyy-MM-dd").endOf(
-								"day",
-							),
-						),
-					),
-				),
+			    uniqBy(periods, "id").filter((pe) =>
+			        period.interval.engulfs(
+			            Interval.fromDateTimes(
+			                DateTime.fromFormat(
+			                    pe.startDate,
+			                    "yyyy-MM-dd",
+			                ).startOf("day"),
+			                DateTime.fromFormat(pe.endDate, "yyyy-MM-dd").endOf(
+			                    "day",
+			                ),
+			            ),
+			        ),
+			    ),
 			) ?? [];
 
-		return filteredPeriods.map((period) => {
-			return {
-				...period,
-				interval: Interval.fromDateTimes(
-					DateTime.fromFormat(period.startDate, "yyyy-MM-dd").startOf(
-						"day",
-					),
-					DateTime.fromFormat(period.endDate, "yyyy-MM-dd").endOf(
-						"day",
-					),
-				),
-				start: DateTime.fromFormat(
-					period.startDate,
-					"yyyy-MM-dd",
-				).startOf("day"),
-				end: DateTime.fromFormat(period.endDate, "yyyy-MM-dd").endOf(
-					"day",
-				),
-			};
-		});
-	}, [period, config]);
+        return filteredPeriods.map((period) => {
+            return {
+                ...period,
+                interval: Interval.fromDateTimes(
+                    DateTime.fromFormat(period.startDate, "yyyy-MM-dd").startOf(
+                        "day",
+                    ),
+                    DateTime.fromFormat(period.endDate, "yyyy-MM-dd").endOf(
+                        "day",
+                    ),
+                ),
+                start: DateTime.fromFormat(
+                    period.startDate,
+                    "yyyy-MM-dd",
+                ).startOf("day"),
+                end: DateTime.fromFormat(period.endDate, "yyyy-MM-dd").endOf(
+                    "day",
+                ),
+            };
+        });
+    }, [period, config]);
 }
 
 export function useTrackingColumns() {
-	const type = usePageType();
+    const type = usePageType();
 
-	const { period } = useDimensions();
+    const { period } = useDimensions();
 
-	const trackingPeriods = useTrackingPeriods();
+    const trackingPeriods = useTrackingPeriods();
 
-	return useMemo(() => {
-		if (type === "planning") {
-			return [
-				{
-					id: `latest-status`,
-					width: 150,
-					name: i18n.t("Latest status"),
-					visible: true,
-					from: "tracking",
-					period,
-				},
-			] as ActionTrackingColumnStateConfig[];
-		} else {
-			return compact(
-				trackingPeriods.map((period) => {
-					const periodObject = period;
-					if (!periodObject) {
-						return;
-					}
-					return {
-						id: periodObject.id,
-						name: periodObject.name,
-						visible: true,
-						width: 0,
-						from: "tracking",
-						period,
-					} as ActionTrackingColumnStateConfig;
-				}),
-			);
-		}
-	}, [type, trackingPeriods]);
+    return useMemo(() => {
+        if (type === "planning") {
+            return [
+                {
+                    id: "latest-status",
+                    width: 150,
+                    name: i18n.t("Latest status"),
+                    visible: true,
+                    from: "tracking",
+                    period,
+                },
+            ] as ActionTrackingColumnStateConfig[];
+        } else {
+            return compact(
+                trackingPeriods.map((period) => {
+                    const periodObject = period;
+                    if (!periodObject) {
+                        return;
+                    }
+                    return {
+                        id: periodObject.id,
+                        name: periodObject.name,
+                        visible: true,
+                        width: 0,
+                        from: "tracking",
+                        period,
+                    } as ActionTrackingColumnStateConfig;
+                }),
+            );
+        }
+    }, [type, trackingPeriods]);
 }
 
 export function useSetColumnState() {
-	const { width } = useWindowSize();
-	const trackingColumns = useTrackingColumns();
-	// This sets the initial state of the columns;
-	const { config } = useConfiguration();
-	const setDefaultColumnState = useSetRecoilState(
-		ColumnState(config?.id as string),
-	);
-	const tableHeaders = useMemo(() => {
-		if (!config) {
-			return [];
-		}
-		const [, ...rest] = config.categories;
-		const categoriesHeaders = rest
-			.map((category) => {
-				return category.fields
-					.filter(({ showAsColumn }) => showAsColumn)
-					.map((field) => ({
-						...field,
-						from: category.id,
-					}));
-			})
-			.flat();
-		const actionsHeaders = config.action.fields
-			.filter(({ showAsColumn }) => showAsColumn)
-			.map((field) => ({
-				...field,
-				from: config.action.id,
-			}));
-		const planningColumns = [...categoriesHeaders, ...actionsHeaders].map(
-			(header) => {
-				return {
-					id: header.id,
-					visible: true,
-					width: 150,
-					name: header.name,
-					from: header.from,
-				} as ColumnStateConfig;
-			},
-		);
+    const { width } = useWindowSize();
+    const trackingColumns = useTrackingColumns();
+    // This sets the initial state of the columns;
+    const { config } = useConfiguration();
+    const setDefaultColumnState = useSetRecoilState(
+        ColumnState(config?.id as string),
+    );
+    const tableHeaders = useMemo(() => {
+        if (!config) {
+            return [];
+        }
+        const [, ...rest] = config.categories;
+        const categoriesHeaders = rest
+            .map((category) => {
+                return category.fields
+                    .filter(({ showAsColumn }) => showAsColumn)
+                    .map((field) => ({
+                        ...field,
+                        from: category.id,
+                    }));
+            })
+            .flat();
+        const actionsHeaders = config.action.fields
+            .filter(({ showAsColumn }) => showAsColumn)
+            .map((field) => ({
+                ...field,
+                from: config.action.id,
+            }));
+        const planningColumns = [...categoriesHeaders, ...actionsHeaders].map(
+            (header) => {
+                return {
+                    id: header.id,
+                    visible: true,
+                    width: 150,
+                    name: header.name,
+                    from: header.from,
+                } as ColumnStateConfig;
+            },
+        );
 
-		const columns = [...planningColumns, ...trackingColumns];
+        const columns = [...planningColumns, ...trackingColumns];
 
-		const columnWidth = clamp(100 / columns.length, 10, 100);
+        const columnWidth = clamp(100 / columns.length, 10, 100);
 
-		return columns.map((column) => {
-			return {
-				...column,
-				width: columnWidth,
-			};
-		});
-	}, [config, width, trackingColumns]);
-	useEffect(() => {
-		setDefaultColumnState(tableHeaders);
-	}, [width, trackingColumns]);
+        return columns.map((column) => {
+            return {
+                ...column,
+                width: columnWidth,
+            };
+        });
+    }, [config, width, trackingColumns]);
+    useEffect(() => {
+        setDefaultColumnState(tableHeaders);
+    }, [width, trackingColumns]);
 }
 
 export function useColumns() {
-	const { config } = useConfiguration();
-	return useRecoilValue(VisibleColumnState(config?.id as string));
+    const { config } = useConfiguration();
+    return useRecoilValue(VisibleColumnState(config?.id as string));
 }
 
 export function useManageColumns() {
-	const { config } = useConfiguration();
-	const { width } = useWindowSize();
-	const [columns, setColumns] = useRecoilState(
-		ColumnState(config?.id as string),
-	);
+    const { config } = useConfiguration();
+    const { width } = useWindowSize();
+    const [columns, setColumns] = useRecoilState(
+        ColumnState(config?.id as string),
+    );
 
-	const manageColumns = useCallback(
-		(columns: ColumnStateConfig[]) => {
-			setColumns(columns);
-		},
-		[setColumns, width],
-	);
+    const manageColumns = useCallback(
+        (columns: ColumnStateConfig[]) => {
+            setColumns(columns);
+        },
+        [setColumns, width],
+    );
 
-	return {
-		columns,
-		manageColumns,
-	};
+    return {
+        columns,
+        manageColumns,
+    };
 }
