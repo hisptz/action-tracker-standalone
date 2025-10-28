5b27913271703f77d6f3d66b49c0149eb76b46ad
Gift Nnko <nnkogift@gmail.com>
2025-10-28 15:43:04 +0300

    chore: add app ID and DHIS2 version range to config

--- d2.config.js
+++ d2.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
+	id: "0efc14dc-bf0b-474d-8a02-82c27d891b23",
 	type: "app",
 	title: "Standalone Action Tracker",
 	entryPoints: {
@@ -11,6 +12,8 @@ const config = {
 		"Standalone Action Tracker - Tracking",
 		"Standalone Action Tracker - Configure",
 	],
+	minDHIS2Version: "2.40",
+	maxDHIS2Version: "2.42",
 	viteConfigExtensions: "./viteConfigExtensions.mts",
 };
 module.exports = config;
